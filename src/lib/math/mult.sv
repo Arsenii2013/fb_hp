c0269b3eeec8ccab7b90ed48af61486b23374c3f
Arsenii2013 <a.nepein@g.nsu.ru>
2024-03-22 11:55:04 +0700

    Change altera multiplicator to xilinx multiplicator

--- src/lib/math/mult.sv
+++ src/lib/math/mult.sv
@@ -317,27 +317,41 @@ module mult16x16_m
 //
 //      Instances
 //
-lpm_mult
-#(
-  .lpm_hint           ( "INPUT_A_IS_CONSTANT=NO,INPUT_B_IS_CONSTANT=NO,DEDICATED_MULTIPLIER_CIRCUITRY=YES,MAXIMIZE_SPEED=5" ),
-  .lpm_widtha         ( 16                                                                                                  ),
-  .lpm_widthb         ( 16                                                                                                  ),
-  .lpm_widthp         ( 32                                                                                                  ),
-  .lpm_type           ( "LPM_MULT"                                                                                          ),
-  .lpm_representation ( "UNSIGNED"                                                                                          ),
-  .lpm_pipeline       ( 1                                                                                                   )
-)
-lpm_mult
-(
-  .dataa  ( a    ),
-  .datab  ( b    ),
-  .clken  ( 1'b1 ),
-  .clock  ( clk  ),
-  .result ( p    ),
-  .aclr   ( 1'b0 ),
-  .sclr   ( 1'b0 ),
-  .sum    ( 1'b0 )
-);
+    /*lpm_mult
+    #(
+    .lpm_hint           ( "INPUT_A_IS_CONSTANT=NO,INPUT_B_IS_CONSTANT=NO,DEDICATED_MULTIPLIER_CIRCUITRY=YES,MAXIMIZE_SPEED=5" ),
+    .lpm_widtha         ( 16                                                                                                  ),
+    .lpm_widthb         ( 16                                                                                                  ),
+    .lpm_widthp         ( 32                                                                                                  ),
+    .lpm_type           ( "LPM_MULT"                                                                                          ),
+    .lpm_representation ( "UNSIGNED"                                                                                          ),
+    .lpm_pipeline       ( 1                                                                                                   )
+    )
+    lpm_mult
+    (
+    .dataa  ( a    ),
+    .datab  ( b    ),
+    .clken  ( 1'b1 ),
+    .clock  ( clk  ),
+    .result ( p    ),
+    .aclr   ( 1'b0 ),
+    .sclr   ( 1'b0 ),
+    .sum    ( 1'b0 )
+    );*/
+
+    MULT_MACRO #(
+    .DEVICE("7SERIES"), // Target Device: "7SERIES"
+    .LATENCY(1),        // Desired clock cycle latency, 0-4
+    .WIDTH_A(16),       // Multiplier A-input bus width, 1-25
+    .WIDTH_B(16)        // Multiplier B-input bus width, 1-18
+    ) MULT_MACRO_inst (
+    .P(p),     // Multiplier output bus, width determined by WIDTH_P parameter
+    .A(a),     // Multiplier input A bus, width determined by WIDTH_A parameter
+    .B(b),     // Multiplier input B bus, width determined by WIDTH_B parameter
+    .CE('1),   // 1-bit active high input clock enable
+    .CLK(clk), // 1-bit positive edge clock input
+    .RST('1)  // 1-bit input active high reset
+    );
 
 //------------------------------------------------
 endmodule : mult16x16_m
