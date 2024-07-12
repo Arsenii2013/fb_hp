edd4bdfec39223335278479c074927af53681ba2
Arsenii2013 <a.nepein@g.nsu.ru>
2024-07-12 12:18:04 +0700

    Add EMIO events FIFO. In this way, there is a priblem with asyc EMIO and FCLK

--- src/syn/top.svh
+++ src/syn/top.svh
@@ -6,7 +6,7 @@
 localparam CLK_PRD         = 10;
 localparam SYNC_PRD_DEF    = 1000;
 localparam FB_DW           = 32;
-localparam EMIO_SIZE       = 32;
+localparam EMIO_SIZE       = 64;
 
 
 //      PCI Express
